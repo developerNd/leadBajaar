f3463af774b179f67641ea01944970cdc3df2f0f
developerNd <91623104+developerNd@users.noreply.github.com>
2025-02-02 10:32:03 +0530

    layout updated to sovle hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,14 +11,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider>
           {children}
           <Toaster />
         </ThemeProvider>
