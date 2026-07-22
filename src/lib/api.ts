86958a4bfe307b7879fed2d61b3062cc2c781184
developerNd <91623104+developerNd@users.noreply.github.com>
2026-07-22 13:54:22 +0530

    api url updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,8 +4,8 @@ import { parseError } from '@/utils/errorParser';
 import { logger } from '@/utils/logger';
 
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api'
-// export const API_BASE_URL = 'https://api.leadbajaar.com/api'
-export const API_BASE_URL = 'http://localhost:8000/api'
+export const API_BASE_URL = 'https://api.leadbajaar.com/api'
+// export const API_BASE_URL = 'http://localhost:8000/api'
 // export const WHATSAPP_BASE_URL = 'http://localhost:3000/api'
 export const WHATSAPP_BASE_URL = 'https://wp.leadbajaar.com/api'
 
